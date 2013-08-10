fadca2ae2bb23110cf0d38194d1ef5e6ce9d3bf1
animatinator <animatinator@gmail.com>
2013-08-10 22:10:33 +0100

    Remove IntelliJ comment

--- src/com/compscidave/swishassert/Assert.java
+++ src/com/compscidave/swishassert/Assert.java
@@ -2,13 +2,7 @@ package com.compscidave.swishassert;
 
 import com.compscidave.swishassert.constraints.Constraint;
 
-/**
- * Created with IntelliJ IDEA.
- * User: David
- * Date: 10/08/13
- * Time: 21:52
- * To change this template use File | Settings | File Templates.
- */
+
 public class Assert {
 
     public static final String DEFAULT_FALSE_ASSERTION_MESSAGE = "Expression was false";
