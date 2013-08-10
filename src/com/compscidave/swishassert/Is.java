8f5a78f5393d8a10096dc44d11f3a71a0da7cfc3
animatinator <animatinator@gmail.com>
2013-08-10 21:21:38 +0100

    Initial commit

---
+++ src/com/compscidave/swishassert/Is.java
@@ -0,0 +1,11 @@
+package com.compscidave.swishassert;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: David
+ * Date: 10/08/13
+ * Time: 21:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Is {
+}
---
+++ src/com/compscidave/swishassert/test/MessingAround.java
@@ -0,0 +1,19 @@
+package com.compscidave.swishassert.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: David
+ * Date: 10/08/13
+ * Time: 21:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessingAround {
+    @Test
+    public void testTest() {
+        fail("Faff");
+    }
+}
