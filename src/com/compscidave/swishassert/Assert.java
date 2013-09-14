b4e776d457d18ef4ba1ac7f0d331a6f4cd4a703c
animatinator <animatinator@gmail.com>
2013-09-14 15:05:31 +0100

    Add Assert.IsTrue and Assert.IsFalse

--- src/com/compscidave/swishassert/Assert.java
+++ src/com/compscidave/swishassert/Assert.java
@@ -6,6 +6,7 @@ import com.compscidave.swishassert.constraints.Constraint;
 public class Assert {
 
     public static final String DEFAULT_FALSE_ASSERTION_MESSAGE = "Expression was false";
+    private static final String DEFAULT_TRUE_ASSERTION_MESSAGE = "Expression was true";
     public static final String DEFAULT_FORCED_FAILURE_MESSAGE = "Forced failure";
 
     public static void That(boolean expression) {
@@ -22,6 +23,22 @@ public class Assert {
                 throw new AssertionError(constraint.getFailureMessage(object));
             }
         }
+    }
+
+    public static void IsTrue(boolean expression, String message) {
+        That(expression, message);
+    }
+
+    public static void IsTrue(boolean expression) {
+        IsTrue(expression, DEFAULT_FALSE_ASSERTION_MESSAGE);
+    }
+
+    public static void IsFalse(boolean expression, String message) {
+        That(expression, message);
+    }
+
+    public static void IsFalse(boolean expression) {
+        IsFalse(expression, DEFAULT_TRUE_ASSERTION_MESSAGE);
     }
 
     public static void Fail() {
